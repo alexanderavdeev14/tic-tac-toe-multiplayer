125fd901081d1886eaf63c54a60bbb903d644777
Alexander Avdeev <a.avdeev.jr@gmail.com>
2022-05-12 21:09:57 +0200

    init

---
+++ src/main/java/alexander/avdeev/games/tictactoemultiplayer/TictactoeMultiplayerApplication.java
@@ -0,0 +1,13 @@
+package alexander.avdeev.games.tictactoemultiplayer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TictactoeMultiplayerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TictactoeMultiplayerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/alexander/avdeev/games/tictactoemultiplayer/TictactoeMultiplayerApplicationTests.java
@@ -0,0 +1,13 @@
+package alexander.avdeev.games.tictactoemultiplayer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TictactoeMultiplayerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
